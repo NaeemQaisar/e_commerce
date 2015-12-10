d9c08a684cfa58029abf2eacf81fbb4069ff95b9
NaeemQaisar <mr.naeem2000@gmail.com>
2015-12-10 09:10:10 -0500

    Product and comments sacffold created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ECommerce::Application.config.secret_token = '006e4eb37f3fe6d7093826f98b5a2a2dc732dbe6eb491f1ef9b81d9a51cfc590ac9a2d972c8774d24d8c7bbef1f4cc3742448a3dc1525d29cbdf8ccff7966b5d'
